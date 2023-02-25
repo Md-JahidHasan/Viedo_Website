7d440ae01789b1fe5af493359f3a93c6cf6fa07f
Md-JahidHasan <md.jahidhasan.seu@gmail.com>
2023-02-25 22:01:47 +0600

    video description page setup

---
+++ src/features/video/videoSlice.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { getVideo } from "./videoAPI"
+
+const initialState = {
+    video: {},
+    isLoading: false,
+    isError: false,
+    error: ''
+}
+
+// thunk function 
+export const fetchVideo = createAsyncThunk('video/fetchVideo', async (id)=>{
+    const video = await getVideo(id);
+    return video;
+})
+
+
+const videoSlice = createSlice({
+    name: 'video',
+    initialState,
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchVideo.pending, (state)=>{
+            state.isLoading = true;
+            state.isError = false;
+        })
+        .addCase(fetchVideo.fulfilled, (state, action)=>{
+            state.isLoading =false;
+            state.video = action.payload;
+        })
+        .addCase(fetchVideo.rejected, (state, action)=>{
+            state.isError = true;
+            state.isLoading = false;
+            state.video = {};
+            state.video = action.error.message;
+        })
+    }
+})
+
+export default videoSlice.reducer;
